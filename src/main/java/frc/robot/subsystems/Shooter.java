17778d2655607fbb5275c1a747b34b5623da16d7
ike <ihschw241@isd709.org>
2022-01-22 13:26:31 -0600

    added feedfoward to shooter

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -1,31 +1,46 @@
 package frc.robot.subsystems;
 
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
 import edu.wpi.first.math.filter.SlewRateLimiter;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.ctre.phoenix.Logger;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
-public class Shooter extends SubsystemBase {  
-    private final NetworkTableEntry m_speed = NetworkTableInstance.getDefault().getTable("Test").getEntry("Speed");
 
-    private final SlewRateLimiter m_limiter;
-    
-    private final int channel = 0;
+public class Shooter extends SubsystemBase { 
+  private final int channel = 0;
+  private double ks = 0;
+  private double kv = 0;
 
-    private final PWMSparkMax m_spark;
+  private final NetworkTableEntry m_speed = NetworkTableInstance.getDefault().getTable("Test").getEntry("Speed");
 
-    public Shooter() {
-        m_spark = new PWMSparkMax(0);
-        
-        m_limiter = new SlewRateLimiter(0.4);
-    
-        m_speed.setDouble(0);
+  private final PWMSparkMax m_spark;
+  private final SlewRateLimiter m_limiter;
+  private final SimpleMotorFeedforward feedforward;
 
-    }
+  public Shooter() {
+    m_speed.setDouble(0);
+
+    m_spark = new PWMSparkMax(channel);
+      
+    m_limiter = new SlewRateLimiter(0.4);
+    feedforward = new SimpleMotorFeedforward(ks, kv);
+  }
 
   public void set(double speed) {
     speed = m_limiter.calculate(speed);
     m_spark.set(speed);
   }  
+  public void setvoltage(double voltage){
+    m_spark.setVoltage(voltage);
+  }
+  
+  public void setRPM (double RPM){
+    double voltage = feedforward.calculate(RPM);
+    m_spark.setVoltage(voltage);
+  }
 }
