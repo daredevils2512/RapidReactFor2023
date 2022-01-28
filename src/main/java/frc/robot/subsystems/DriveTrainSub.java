9938b64cb46ad77257bbf7d36ab8f193c9dd8848
cael brenning <caelbrenning>
2022-01-28 17:51:39 -0600

    endocer finished :) :wq

--- src/main/java/frc/robot/subsystems/DriveTrainSub.java
+++ src/main/java/frc/robot/subsystems/DriveTrainSub.java
@@ -6,34 +6,34 @@ package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
+import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.motorcontrol.MotorController;
 import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
-// import frc.robot.RobotContainer;
 
-// import com.ctre.phoenix.motorcontrol.InvertType;
-
-public class DriveTrainSub extends SubsystemBase {
+public class DriveTrainSub extends PropertiesSubsystem {
   // private WPI_TalonSRX fr, br, fl, bl;
   private DifferentialDrive m_drive;
 
-  double maxSpeed = 1;
-  double maxTurn = 1;
+  double maxSpeed = 0.5;
+  double maxTurn = 0.5;
 
-
-  /** Creates a new ExampleSubsystem. */
+  /* Creates a new ExampleSubsystem. */
   public DriveTrainSub() {
+    super("DriveTrainSub");
     MotorController m_frontLeft = new WPI_TalonSRX(13);
     MotorController m_backLeft = new WPI_TalonSRX(12);
     MotorControllerGroup m_left = new MotorControllerGroup(m_frontLeft, m_backLeft);
+    m_left.setInverted(true);
+    
+    Encoder m_leftEncoder = new Encoder(0, 1);
 
     MotorController m_frontRight = new WPI_TalonSRX(10);
     MotorController m_backRight = new WPI_TalonSRX(11);
     MotorControllerGroup m_right = new MotorControllerGroup(m_frontRight, m_backRight);
-    
-    m_left.setInverted(true);
 
+    Encoder m_rightEncoder = new Encoder(0, 1);
+  
     m_drive = new DifferentialDrive(m_left, m_right);
   }
 
