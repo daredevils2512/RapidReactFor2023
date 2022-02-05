3632a1b358a09c00a4e6130d0445d228db0670c6
TrollBlox <caelbrenning@gmail.com>
2022-02-04 20:40:30 -0600

    i forgot to add a file to the previous commit

---
+++ src/main/java/frc/robot/commands/IntakeCommand.java
@@ -0,0 +1,27 @@
+package frc.robot.commands;
+
+import java.util.function.DoubleSupplier;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.IntakeSub;
+
+public class IntakeCommand extends CommandBase{
+  // Variables
+  private final IntakeSub m_intake;
+  private final DoubleSupplier m_speed;
+
+  /** Assigns the values of the arcadeDrive stuff 
+   * @param IntakeSub The subsystem to use
+   * @param DoubleSupplier Control for speed of intake
+  */
+  public IntakeCommand(IntakeSub intake, DoubleSupplier speed) {
+    m_intake = intake;
+    m_speed = speed;
+    addRequirements(m_intake);
+  }  
+
+  /** Executes intake code */
+  public void execute() {
+    m_intake.setIntake(m_speed.getAsDouble());
+  }
+}
