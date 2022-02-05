89c80b55f1f900ef6b91a26b06e3c68528e7fc67
toby <tobykothe@gmail.com>
2022-02-05 10:24:35 -0600

    fixed spacing

--- src/main/java/frc/robot/subsystems/Magazine.java
+++ src/main/java/frc/robot/subsystems/Magazine.java
@@ -3,19 +3,19 @@ package frc.robot.subsystems;
 import edu.wpi.first.wpilibj.motorcontrol.Victor;
 
 public class Magazine extends NTSubsystem {
-    private final Victor m_magMotor;
-    private final int m_magMotorID = 69;
+  private final Victor m_magMotor;
+  private final int m_magMotorID = 69;
 
-    public Magazine() {
-        super("Magazine");
-        // the channel below is not an actual channel, and therefore just a placeholder. 
-         m_magMotor = new Victor(m_magMotorID);
+  public Magazine() {
+    super("Magazine");
+    // the channel below is not an actual channel, and therefore just a placeholder. 
+    m_magMotor = new Victor(m_magMotorID);
 
-    }
+  }
 
-    public void moveBalls (double magSpeed) {
-        m_magMotor.set(magSpeed);
-        m_logger.fine("move balls: " + m_magMotor.get());
-    }
+  public void moveBalls (double magSpeed) {
+    m_magMotor.set(magSpeed);
+    m_logger.fine("move balls: " + m_magMotor.get());
+  }
 
 }
