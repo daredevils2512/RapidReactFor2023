162de0f9ac1e62e533b27d59a3a2e66270d6fac8
TrollBlox <caelbrenning@gmail.com>
2022-02-14 20:18:56 -0600

    Constants

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -9,31 +9,11 @@ import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.Encoder;
-import edu.wpi.first.wpilibj.PneumaticsModuleType;
-import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
+import frc.robot.utils.Constants;
 
 public class Drivetrain extends NTSubsystem {
-
-  // IDs TODO: change to correct values!
-  private final int k_frontLeftID = 00;
-  private final int k_backLeftID = 01;
-  private final int k_frontRightID = 02;
-  private final int k_backRightID = 03;
-  private final int k_leftEncoderID1 = 0;
-  private final int k_leftEncoderID2 = 1;
-  private final int k_rightEncoderID1 = 0;
-  private final int k_rightEncoderID2 = 1;
-  private final double k_rateLimNUM = 0.5;
-  private final double k_maxSpeed = 0.5;
-  private final double k_maxTurn = 0.5;
-  private final int k_leftForwardChannel = 0;
-  private final int k_leftBackwardChannel = 0;
-  private final int k_rightForwardChannel = 0;
-  private final int k_rightBackwardChannel = 0;
-  private final PneumaticsModuleType k_pneumaticsModuleType = PneumaticsModuleType.CTREPCM;
-
   // Motor stuff
   private final WPI_TalonFX m_frontLeft; 
   private final WPI_TalonFX m_backLeft; 
@@ -71,19 +51,19 @@ public class Drivetrain extends NTSubsystem {
     m_table = NetworkTableInstance.getDefault().getTable("Drive Train");
 
     // Motor stuff
-    m_frontLeft = new WPI_TalonFX(k_frontLeftID); 
-    m_backLeft = new WPI_TalonFX(k_backLeftID);
+    m_frontLeft = new WPI_TalonFX(Constants.drivetrainLeft1ID); 
+    m_backLeft = new WPI_TalonFX(Constants.drivetrainLeft2ID);
     m_left = new MotorControllerGroup(m_frontLeft, m_backLeft);
     m_left.setInverted(true);
-    m_frontRight = new WPI_TalonFX(k_frontRightID);
-    m_backRight = new WPI_TalonFX(k_backRightID);
+    m_frontRight = new WPI_TalonFX(Constants.drivetrainRight1ID);
+    m_backRight = new WPI_TalonFX(Constants.drivetrainRight2ID);
     m_right = new MotorControllerGroup(m_frontRight, m_backRight);
     m_drive = new DifferentialDrive(m_left, m_right); 
 
     // Network table stuff
     m_properties = new Properties();
-    m_leftEncoder = new Encoder(k_leftEncoderID1, k_leftEncoderID2);
-    m_rightEncoder = new Encoder(k_rightEncoderID1, k_rightEncoderID2);
+    m_leftEncoder = new Encoder(Constants.drivetrainLeftEncoderID1, Constants.drivetrainLeftEncoderID2);
+    m_rightEncoder = new Encoder(Constants.drivetrainRightEncoderID1, Constants.drivetrainRightEncoderID2);
     m_leftEncoderEntry = m_table.getEntry("Left encoder distance"); 
     m_rightEncoderEntry = m_table.getEntry("Right encoder distance");
     m_encoderResolution = Integer.parseInt(m_properties.getProperty("encoderResolution"));
@@ -99,11 +79,11 @@ public class Drivetrain extends NTSubsystem {
     m_getLowGearEntry = m_table.getEntry("Low gear entry");
 
     // Shifting
-    m_leftShifter = new DoubleSolenoid(k_pneumaticsModuleType, k_leftForwardChannel, k_leftBackwardChannel);
-    m_rightShifter = new DoubleSolenoid(k_pneumaticsModuleType, k_rightForwardChannel, k_rightBackwardChannel);
+    m_leftShifter = new DoubleSolenoid(Constants.CTREPCM_PneumaticsModuleType, Constants.drivetrainLeftForwardChannel, Constants.drivetrainLeftBackwardChannel);
+    m_rightShifter = new DoubleSolenoid(Constants.CTREPCM_PneumaticsModuleType, Constants.drivetrainRightForwardChannel, Constants.drivetrainRightBackwardChannel);
 
     // Rate limiter
-    m_rateLim = new SlewRateLimiter(k_rateLimNUM);
+    m_rateLim = new SlewRateLimiter(Constants.drivetrainRateLimNUM);
   }
 
   /** Runs the arcade drive 
@@ -113,7 +93,7 @@ public class Drivetrain extends NTSubsystem {
   public void arcadeDrive(double move, double turn) { 
     move = m_rateLim.calculate(move);
     turn = m_rateLim.calculate(turn);
-    m_drive.arcadeDrive((move)*k_maxSpeed, (turn)*k_maxTurn);
+    m_drive.arcadeDrive((move)*Constants.drivetrainMaxSpeed, (turn)*Constants.drivetrainMaxTurn);
   }
 
   /** 
@@ -148,15 +128,15 @@ public class Drivetrain extends NTSubsystem {
    * @param wantsLowGear if it wants to set low gear
    */
   public void setLowGear(boolean wantsLowGear) {
-    m_leftShifter.set(wantsLowGear ? Value.kForward : Value.kReverse);
-    m_rightShifter.set(wantsLowGear ? Value.kForward : Value.kReverse);
+    m_leftShifter.set(wantsLowGear ? Constants.value_kforward : Constants.value_kreverse);
+    m_rightShifter.set(wantsLowGear ? Constants.value_kforward : Constants.value_kreverse);
     m_logger.fine("set low gear: " + wantsLowGear);
   }
 
   /** @return true if shifter are in low gear */
   public boolean getLowGear() {
-    m_logger.fine("get low gear: " + (m_leftShifter.get() == Value.kForward ? true : false));
-    return m_leftShifter.get() == Value.kForward ? true : false;
+    m_logger.fine("get low gear: " + (m_leftShifter.get() == Constants.value_kforward ? true : false));
+    return m_leftShifter.get() == Constants.value_kforward ? true : false;
   }
 
   public void toggleShifters() {
--- src/main/java/frc/robot/utils/Constants.java
+++ src/main/java/frc/robot/utils/Constants.java
@@ -1,8 +1,7 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot.utils;
+
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
@@ -14,10 +13,55 @@ package frc.robot.utils;
  */
 public final class Constants {
   
-  //TODO set auto speed
+  // TODO set auto speed
   public static final double DRIVE_AUTO_SPEED = 0.5;
-  //TODO change distance
+  // TODO change distance
   public static final double AUTO_DRIVE_BACK_DISTANCE = 150;
+
+  // Drivetrain
+  public static double drivetrainRateLimNUM = 0.5;
+  public static double drivetrainMaxSpeed = 0.5;
+  public static double drivetrainMaxTurn = 0.5;
+  public static int drivetrainLeftForwardChannel = 0;
+  public static int drivetrainLeftBackwardChannel = 0;
+  public static int drivetrainRightForwardChannel = 0;
+  public static int drivetrainRightBackwardChannel = 0;
+
+  // Intake
+  public static int intakeShifter1ForwardID = 01;
+  public static int intakeShifter1BackwardID = 01;
+  public static int intakeShifter2ForwardID = 02;
+  public static int intakeShifter2BackwardID = 02;
+
+  // Shooter
+  public static int shooterEncoderID1 = 1;
+  public static int shooterEncoderID2 = 2;
+  public static double shooterRateLimNUM = 0.5;
+  public static int shooterForwardChannel = 0;
+  public static int shooterBackwardChannel = 1;
+
+  // Motor IDs
+  public static int drivetrainLeft1ID = 0;
+  public static int drivetrainLeft2ID = 1;
+  public static int drivetrainRight1ID = 2;
+  public static int drivetrainRight2ID = 3;
+  public static int intake1ID = 4;
+  public static int intake2ID = 5;
+  public static int magID = 6;
+  public static int climber1ID = 7;
+  public static int climber2ID = 8;
+  public static int shooterID = 9;
+
+  // Encoder IDs
+  public static int drivetrainLeftEncoderID1 = 0;
+  public static int drivetrainLeftEncoderID2 = 1;
+  public static int drivetrainRightEncoderID1 = 0;
+  public static int drivetrainRightEncoderID2 = 1;
+
+  // Other stuffs
+  public static PneumaticsModuleType CTREPCM_PneumaticsModuleType = PneumaticsModuleType.CTREPCM;
+  public static Value value_kforward = Value.kForward;
+  public static Value value_kreverse = Value.kReverse;
 
   private Constants(){
 
