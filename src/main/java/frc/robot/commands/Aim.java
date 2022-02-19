147bff9c957c272bea891b12c4f974441290c64a
NOAH <smurfmuchies@gmail.com>
2022-02-19 10:53:32 -0600

    finished Aim.java need to add range finder.

--- src/main/java/frc/robot/commands/Aim.java
+++ src/main/java/frc/robot/commands/Aim.java
@@ -1,6 +1,5 @@
 package frc.robot.commands;
 
-import java.util.function.DoubleSupplier;
 
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
@@ -10,20 +9,18 @@ import frc.robot.subsystems.Drivetrain;
 
 public class Aim extends CommandBase{
   private final Drivetrain m_drivetrain;
-  private final Double m_speed;
   private final NetworkTable m_limelight;
   private final NetworkTableEntry m_tx; 
   private final Double m_aimAjust; 
   private final Double Kp; 
 
 
-  public Aim(Drivetrain drivetrain, Double speed){
+  public Aim(Drivetrain drivetrain){
     m_drivetrain = drivetrain; 
-    Kp = 0.0;
-    m_speed = speed;
+    Kp = 0.2;
     m_limelight = NetworkTableInstance.getDefault().getTable("limelight");
     m_tx = m_limelight.getEntry("tx");
-    m_aimAjust = Kp * m_tx.getDouble(0);
+    m_aimAjust = Kp * m_tx.getDouble(0);  
 
   }
 
@@ -34,7 +31,7 @@ public class Aim extends CommandBase{
   /** Called every time the scheduler runs while the command is scheduled. */
   @Override
   public void execute() {
-   
+   m_drivetrain.arcadeDrive(0, m_aimAjust);
   }
 
   /** Called once the command ends or is interrupted. */
