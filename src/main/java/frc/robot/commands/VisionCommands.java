1b8fe80fe6cd1e87b4d25392ccdbcdd38ad5304b
TrollBlox <caelbrenning@gmail.com>
2022-03-15 20:29:33 -0500

    added comments for commands

--- src/main/java/frc/robot/commands/VisionCommands.java
+++ src/main/java/frc/robot/commands/VisionCommands.java
@@ -12,15 +12,24 @@ import frc.robot.vision.LimelightLEDMode;
 public final class VisionCommands {
   private VisionCommands() {}
 
+  // Limelight network table
   static NetworkTable m_limelightTable = NetworkTableInstance.getDefault().getTable("limelight");
 
+  /** Aims the robot automatically
+   * @param drivetrian The drivetrain subsystem to use.
+   * @return The command to be used when called.
+   */
   public static Command Aim(Drivetrain drivetrain) {
     double tx = m_limelightTable.getEntry("tx").getDouble(0);
     double turnAdjust = Constants.K_P * tx;
 
-    return new RunCommand(() -> drivetrain.arcadeDrive(0, turnAdjust));
+    return Commands.drive(drivetrain, () -> 0.0, () -> turnAdjust);
   }
 
+  /** Automatically moves the robot to a certain distance away from the goalpost
+   * @param drivetrain The drivetrain subsystem to use.
+   * @return The command to be used when called.
+   */
   public static Command findRange(Drivetrain drivetrain) {
     double ty = m_limelightTable.getEntry("ty").getDouble(0);
     double angleToGoalDegrees = Constants.LIMELIGHT_MOUNT_ANGLE_DEGREES + ty;
@@ -29,13 +38,21 @@ public final class VisionCommands {
     double distanceVariation = Constants.DESIRED_DISTANCE - currentDistance;
     double moveAjust = Constants.K_P * distanceVariation;
 
-    return new RunCommand(() -> drivetrain.arcadeDrive(moveAjust, 0));
+    return Commands.drive(drivetrain, () -> moveAjust, () -> 0.0);
   }
 
+  /** Command that turns the limelight on
+   * @param limelight The limelight subsystem to use.
+   * @return The command to be used when called.
+   */
   public static Command turnOnLimelight(Limelight limelight) {
     return new RunCommand(() -> limelight.setLEDMode(LimelightLEDMode.ON));
   }
 
+  /** Command that turns the limelight off
+   * @param limelight The limelight subsystem to use.
+   * @return The command to be used when called.
+   */
   public static Command turnOffLimelight(Limelight limelight) {
     return new RunCommand(() -> limelight.setLEDMode(LimelightLEDMode.OFF));
   }
